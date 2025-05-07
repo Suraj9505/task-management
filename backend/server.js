e151f804bed0ddf9712b5984abc1dc621c356db9
Suraj Jha <sjha5505@gmail.com>
2025-05-07 20:43:02 +0530

    --separated db connection code

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import cors from 'cors';
 
 import authRoutes from './routes/auth.js';
 import taskRoutes from './routes/tasks.js';
+import connectToMongoDB from './utils/connectToDb.js';
 
 dotenv.config();
 
@@ -17,9 +18,8 @@ app.use('/api/tasks', taskRoutes);
 
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then(() => {
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-}).catch(err => console.error(err));
+
+app.listen(PORT, () => {
+    connectToMongoDB();
+    console.log(`Server running on port ${PORT}`)
+});
